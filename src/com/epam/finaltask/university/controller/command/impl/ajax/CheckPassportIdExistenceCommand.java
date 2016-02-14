e2be2c78f8436bccbdebe87e087207fdc12ac22d
Evgeniy Chaichits <zhenyachay@gmail.com>
2016-02-14 15:33:57 +0300

    Bug fixes

---
+++ src/com/epam/finaltask/university/controller/command/impl/ajax/CheckPassportIdExistenceCommand.java
@@ -0,0 +1,7 @@
+package com.epam.finaltask.university.controller.command.impl.ajax;
+
+/**
+ * Created by Zheny Chaichits on 14.02.2016.
+ */
+public class CheckPassportIdExistenceCommand {
+}
---
+++ src/com/epam/finaltask/university/service/concurrent/LockingProfileService.java
@@ -0,0 +1,7 @@
+package com.epam.finaltask.university.service.concurrent;
+
+/**
+ * Created by Zheny Chaichits on 14.02.2016.
+ */
+public class LockingProfileService {
+}
